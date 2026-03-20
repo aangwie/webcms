cf83d8b4c3f030bcfba99080913788ef8de5052d
aangwie <wirawan.aang5>
2026-03-20 16:21:20 +0700

    Modify filesystem.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => is_dir(base_path('../public_html')) ? base_path('../public_html/storage') : storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
